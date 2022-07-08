7e68e9a7867ad16189a7b7935c0c308ec77851db
nhatnvph15151 <nnhat1802@gmail.com>
2022-07-08 15:57:04 +0700

    feat: update

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
+// scroll menu
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("stick", window.scrollY > 0);
 })
+// back to top
+let mybutton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 50
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
